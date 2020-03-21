bc72420d4241f161a27b5b76b5fceb52dedf6932
bvnger <stevensinfo27@gmail.com>
2020-03-21 02:09:19 -0700

    first commit

---
+++ src/components/todos-list.component.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+export default class TodosList extends Component {
+    render() {
+        return (
+            <div>
+                <p> Welcome to DoinStuffs, Let's do things!</p>
+            </div>
+        )
+    }
+}
